f6652403f7930a6b2af151a5e47b4b73b6ba1271
Khamzaev <khamzaew.j@gmail.com>
2025-03-09 20:08:25 +0300

    api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
@@ -13,12 +11,6 @@ router_v1.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path(
-        'v1/api/token/', TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'v1/api/token/refresh/', TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
